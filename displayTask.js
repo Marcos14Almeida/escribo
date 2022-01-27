bfc5ea7fd779e679d29d7073b621b2b456fb887c
Marcos14Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-01-27 13:41:05 -0300

    Update displayTask.js

--- displayTask.js
+++ displayTask.js
@@ -1,22 +1,32 @@
 displayTask();
 
 function displayTask(){
+    
  var input = prompt("A função a seguir recebe um número inteiro positivo e retorna um somatório de todos os valores inteiros divisíveis por 3 ou 5 e que sejam inferiores ao número digitado.\n\nDigite um número: ");
 
+    //Se clicar em cancelar
+    if (input === null) {
+        return;
+    }
+    
  //Enquanto não digitar um input correto, ou seja, um valor numérico e maior que 0, mostra que houve erro.
  while (input !== parseInt(input, 10).toString() || input<=0) {
     alert("Você não digitou um número válido!!!");
     input = prompt("Digite um número maior que 0: ");
+    //Se clicar em cancelar sai da função antes
+    if (input === null) {
+        return;
+    }
  }
 
- //print Resultado final
- alert(calculate(input));
+  //print Resultado final
+  alert(calculate(input));
 
- //TENTAR DE NOVO
- let isExecuted = confirm("Deseja tentar de novo?");
- if(isExecuted){
+  //TENTAR DE NOVO
+  let isExecuted = confirm("Deseja tentar de novo?");
+  if(isExecuted){
     displayTask();
- }
+  }
 }
 
 
